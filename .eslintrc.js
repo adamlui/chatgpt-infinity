3f48194001430821645afe4df81fe9871f9228c4
Adam Lui 刘展鹏 <adamlui@protonmail.com>
2023-06-20 16:43:30 -0700

    Added `syncExtension` to `globals`

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,7 @@ module.exports  = {
         'no-empty': 'off', // allow empty blocks
         'no-useless-escape': 'off' // allow all escape chars cause ESLint sucks at detecting truly useless ones
     },
-    'globals': { 'chatgpt': 'readonly', 'chrome': 'readonly' },
+    'globals': { 'chatgpt': 'readonly', 'chrome': 'readonly', 'syncExtension': 'readonly' },
     'parserOptions': { 'ecmaVersion': 2022, 'sourceType': 'script' },
     'overrides': [{ 'files': ['**/lib*/**.js'], 'parserOptions': { 'sourceType': 'module' }}],
     'env': { 'browser': true, 'node': true, 'es6': true, 'greasemonkey': true }
