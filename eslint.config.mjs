f9fbfec37e1b0900d3b2de5b67550a37d1fa309f
Adam Lui <adam@kudoai.com>
2024-12-01 01:39:07 -0800

    Added `icons` to JS globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,8 @@ export default [
             ecmaVersion: 'latest', sourceType: 'script',
             globals: {
                 ...globals.browser, ...globals.greasemonkey, ...globals.node, chatgpt: 'readonly', chrome: 'readonly',
-                config: 'writable', dom: 'readonly', modals: 'readonly', settings: 'writable', sidebarToggle: 'writable'
+                config: 'writable', dom: 'readonly', icons: 'writable', modals: 'readonly', settings: 'writable',
+                sidebarToggle: 'writable'
             }
         },
         plugins: { regexp, 'js-styles': stylisticJS },
