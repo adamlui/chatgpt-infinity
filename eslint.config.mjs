270876621ead6f80ce34ef9c402b0c5769ede142
Adam Lui <adam@kudoai.com>
2024-12-01 01:30:54 -0800

    Added `modals` to JS globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ export default [
             ecmaVersion: 'latest', sourceType: 'script',
             globals: {
                 ...globals.browser, ...globals.greasemonkey, ...globals.node, chatgpt: 'readonly', chrome: 'readonly',
-                config: 'writable', dom: 'readonly', settings: 'writable', sidebarToggle: 'writable'
+                config: 'writable', dom: 'readonly', modals: 'readonly', settings: 'writable', sidebarToggle: 'writable'
             }
         },
         plugins: { regexp, 'js-styles': stylisticJS },
